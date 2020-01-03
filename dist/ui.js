ffa0f387fe5c36e9bb8bb9034a46c0e554fe976b
rjrodger <richard@ricebridge.com>
2020-01-03 04:56:59 +0000

    using @seneca/inquirer

--- dist/ui.js
+++ dist/ui.js
@@ -13,7 +13,7 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
 var _ = require('lodash');
-var inquirer = require('inquirer');
+var inquirer = require('@seneca/inquirer');
 var EventEmitter = require('events').EventEmitter;
 var chalk = require('chalk');
 var util = require('./util');
